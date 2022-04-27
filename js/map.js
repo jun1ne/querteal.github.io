374a707fecebec4481ba1cf7f8d96d7148db9e85
QuerTeal <swworld1@gmail.com>
2022-04-27 22:14:53 +0900

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,7 +1,30 @@
-var container = document.getElementById('map');
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.585135, 126.924890), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 지도를 표시하는 div 크기를 변경하는 함수입니다
+function resizeMap() {
+    var mapContainer = document.getElementById('map');
+    mapContainer.style.width = '650px';
+    mapContainer.style.height = '650px'; 
+}
+
+function relayout() {    
+    
+    // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
+    // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다 
+    // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
+    map.relayout();
+}
+
+/* var container = document.getElementById('map');
 var options = {
   center: new kakao.maps.LatLng(37.585135, 126.924890),
-  level: 2
+  level: 3
 };
 
-var map = new kakao.maps.Map(container, options);
+var map = new kakao.maps.Map(container, options); */
