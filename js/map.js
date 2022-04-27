724941f96f35e7bacb88cdd78d4221040e3e1195
QuerTeal <swworld1@gmail.com>
2022-04-28 02:29:54 +0900

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
 var container = document.getElementById('map'),
     options = {
-         center: new kakao.maps.LatLng(33.450701, 126.570667),
+         center: new kakao.maps.LatLng(37.5850969086744, 126.9248570657878),
          level: 3
     };
 var map = new kakao.maps.Map(container, options);
 
-container.style.width = '1200px';
-container.style.height = '800px';
+container.style.width = '500px';
+container.style.height = '400px';
 
 map.relayout();
