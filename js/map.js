2fcc457c73e0426fd831b90436839762aae977b3
QuerTeal <swworld1@gmail.com>
2022-04-28 10:09:11 +0900

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -12,3 +12,14 @@ function relayout() {
     map.relayout();
 }
 //map.setCenter(new DocumentFragment.maps.LatLng(37.5850969086744, 126.9248570657878))
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(37.5850969086744, 126.9248570657878); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
