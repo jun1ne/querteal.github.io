2d7b2db6ecace772e458ff5fc3125239def5466b
QuerTeal <swworld1@gmail.com>
2022-04-28 13:02:47 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,10 +3,6 @@ var container = document.getElementById('map'),
          center: new kakao.maps.LatLng(37.5850969086744, 126.9248570657878),
          level: 3
     };
-var map = new kakao.maps.Map(container, options);
-
-container.style.width = '500px';
-container.style.height = '400px';
 
 function relayout() {    
     map.relayout();
